b82fbaf46ce4e6c1126f54b0e6b1c0ae877f35e6
wissal488 <wissalajb@gmail.com>
2019-11-05 16:35:39 +0000

    js2

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var player1=document.querySelector('.panel-joueur-1');
 var de=document.querySelector('.de');
 var lance=document.querySelector('.btn-lancer');
 var passe=document.querySelector('.btn-passe');
+var nouveau=document.querySelector('.btn-new');
 //var activePlayer = [0,1];
 
 var score=0;
@@ -31,7 +32,7 @@ function init(){
 //Question 2:
 lance.addEventListener("click",function(){
 	de.style="display : yes"
-	var nbr_aleatoire = Math.floor(Math.random()*6)+1;
+	let nbr_aleatoire = Math.floor(Math.random()*6)+1;
 	
 	switch(nbr_aleatoire){
 		case 1 : de.setAttribute('src','de-1.png');score+=1;break;
@@ -51,21 +52,46 @@ lance.addEventListener("click",function(){
  
 
 });
-function joueurSuivant(){
-	de.style="display : none";
-	player0.classList.remove("actif");
-	player1.classList.add("actif");
+/*function joueurSuivant(){
+	
 
 
- /* activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
   score = 0;
 
   score_joueur_courent1.textContent = '0';
   score_joueur_courent2.textContent = '0';
 
   player0.classList.toggle('actif');
-  player1.classList.toggle('actif');*/
-}
+  player1.classList.toggle('actif');
+}*/
 
-passe.addEventListener('click',joueurSuivant());
+passe.addEventListener("click",function joueurSuivant(){
+	de.style="display : none";
+	player0.classList.remove("actif");
+	player1.classList.add("actif");
+	let nbr_aleatoire = Math.floor(Math.random()*6)+1;
+	
+	switch(nbr_aleatoire){
+		case 1 : de.setAttribute('src','de-1.png');score+=1;break;
+		case 2 : de.setAttribute('src','de-2.png');score+=2;break;
+		case 3 : de.setAttribute('src','de-3.png');score+=3;break;
+		case 4 : de.setAttribute('src','de-4.png');score+=4;break;
+		case 5 : de.setAttribute('src','de-5.png');score+=5;break;
+		case 6 : de.setAttribute('src','de-6.png');score+=6;break;
+	}
+	
 
+	if(nbr_aleatoire!=1){
+		score_joueur_courent1.innerText=score;score_joueur1.innerText=score;
+	}
+	
+	else if(nbr_aleatoire==1){score_joueur_courent1.innerText=0;score=0;} 
+ 
+
+});
+nouveau.addEventListener("click",init());
+
+//question 7
+
+var ZoneTxt = document.querySelector('')
